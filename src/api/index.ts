be311784c4788cadfaa655858c801fc34d1ade1d
DamirAkzhigitov <damir.ckelet@gmail.com>
2023-08-05 22:52:21 +0300

    credentials

--- src/api/index.ts
+++ src/api/index.ts
@@ -4,7 +4,8 @@ const instance = axios.create({
   baseURL:
     import.meta.env.MODE === 'development'
       ? '/api/'
-      : 'https://6h68rbyn3i.execute-api.eu-north-1.amazonaws.com/dev/'
+      : 'https://6h68rbyn3i.execute-api.eu-north-1.amazonaws.com/dev/',
+  withCredentials: true
 })
 
 export default instance
